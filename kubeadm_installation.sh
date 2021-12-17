0318e704fe1f3a8de022484af1ce1412303d5d25
59nezytic <tmdgus951116@gmail.com>
2021-12-17 21:09:16 +0900

    '21/12/17'

--- kubeadm_installation.sh
+++ kubeadm_installation.sh
@@ -70,3 +70,15 @@ kubeadm version
 kubelet --version
 
 echo -e "####################### kubeadm installation finished ! #######################\n####################### Set your Master Node & Worker Node #######################"
+echo -e "/////////////////////// MASTER NODE ///////////////////////"
+echo -e "sudo kubeadm init --pod-network-cidr=10.244.0.0/16 --apiserver-advertise-address={__REPLACE_WITH_PRIVATE_IP__MASTER}"
+echo -e "mkdir -p $HOME/.kube"
+echo -e "sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config"
+echo -e "sudo chown $(id -u):$(id -g) $HOME/.kube/config"
+echo -e "kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml"
+echo -e "kubeadm token list"
+echo -e "openssl x509 -pubkey -in /etc/kubernetes/pki/ca.crt | openssl rsa -pubin -outform der 2>/dev/null | openssl dgst -sha256 -hex | sed 's/^.* //'"
+
+
+echo -e "/////////////////////// WORKER NODE ///////////////////////"
+echo -e "sudo kubeadm join {__REPLACE_WITH_PRIVATE_IP__MASTER}:6443 --token {TOKEN_NUM} --discovery-token-ca-cert-hash sha256:{HASH_NUM}"
